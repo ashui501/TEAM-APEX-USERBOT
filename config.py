c56868111af5a66d84ba79c6bc1abd1bf06fb6a1
kira <87417105+SIAmKira@users.noreply.github.com>
2024-02-26 09:17:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ OWNER_ID = os.getenv("OWNER_ID",1238234357)
 HANDLER = ["~", ".","!","?","@","$"]
 BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
-SOURCE = "https://github.com/TeamX-Coders/PyroX-V2"
+SOURCE = "https://github.com/SIAmKira/PyroX-V2"
 LOG_CHAT = -1002119409366
