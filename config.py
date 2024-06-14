39357abb837399f833b36dfd7301e157b85867b2
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-06-14 21:01:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,3 @@ BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
 SOURCE = "https://github.com/SIAmKira/PyroX-V2"
 LOG_CHAT = -1002119409366
-SUDOS = getenv("SUDO_USERS", None)
-SUDO_USERS = []
-if SUDOS:
-    sudos = str(SUDOS).split(" ")
-    for sudo_id in sudos:
-        SUDO_USERS.append(int(sudo_id))
-SUDO_USERS.append(OWNER_ID)
-for x in Barath:
-    SUDO_USERS.append(x)
