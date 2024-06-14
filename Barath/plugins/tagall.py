184726be35e38036bb00c4515fa41f7c40e6d2f6
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-06-14 12:44:00 +0530

    Update tagall.py

--- Barath/plugins/tagall.py
+++ Barath/plugins/tagall.py
@@ -4,8 +4,6 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-from Zaid.modules.help import add_command_help
-
 spam_chats = []
 
 
@@ -17,7 +15,7 @@ def get_arg(message: Message):
         return ""
     return " ".join(split[1:])
 
-@Client.on_message(filters.command("tagall", ".") & filters.me)
+@Client.on_message(filters.command("tag", ".") & filters.me)
 async def mentionall(client: Client, message: Message):
     chat_id = message.chat.id
     direp = message.reply_to_message
@@ -48,7 +46,7 @@ async def mentionall(client: Client, message: Message):
         pass
 
 
-@Client.on_message(filters.command("cancel", ".") & filters.me)
+@Client.on_message(filters.command("c", ".") & filters.me)
 async def cancel_spam(client: Client, message: Message):
     if not message.chat.id in spam_chats:
         return await message.edit("**It seems there is no tagall here.**")
