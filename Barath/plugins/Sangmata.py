357a91dc0011e9887537b9eb3c01e1e25b8f29eb
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-06-18 22:49:13 +0530

    Update Sangmata.py

--- Barath/plugins/Sangmata.py
+++ Barath/plugins/Sangmata.py
@@ -8,7 +8,6 @@ from pyrogram.types import *
 
 @Client.on_message(filters.command(["sg", "sa", "sangmata"], ".") & filters.me)
 async def sg(client: Client, message: Message):
-    args = await extract_user(message)
     lol = await message.edit_text("`Processing...`")
     if args:
         try:
