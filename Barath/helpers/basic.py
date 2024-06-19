345721c8720f4abbf515775b63438faff5610088
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2024-06-19 10:28:29 +0530

    Create basic.py

---
+++ Barath/helpers/basic.py
@@ -0,0 +1,13 @@
+from pyrogram.types import Message
+
+
+async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
+    apa = (
+        message.edit_text
+        if bool(message.from_user and message.from_user.is_self or message.outgoing)
+        else (message.reply_to_message or message).reply_text
+    )
+    return await apa(*args, **kwargs)
+
+
+eor = edit_or_reply
