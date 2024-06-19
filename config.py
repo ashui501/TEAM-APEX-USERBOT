66389fc80044e2127a78e2c45eb036c8cf481af6
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2024-06-19 10:31:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 PREFIXES = ['!', '/', '$'] 
    
 OWNER_ID = os.getenv("OWNER_ID",1238234357)
+SUDO_USERS = os.getenv("SUDO_USERS",1238234357)
 HANDLER = ["~", ".","!","?","@","$"]
 BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
