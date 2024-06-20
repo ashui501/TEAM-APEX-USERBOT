61ed53aaf7660af00dc941e3213bbfc29223dc4b
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-06-20 13:08:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ HANDLER = ["~", ".","!","?","@","$"]
 BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
 SOURCE = "https://github.com/SIAmKira/PyroX-V2"
-LOG_CHAT = -1002119409366
+LOG_GROUP = -1002119409366
