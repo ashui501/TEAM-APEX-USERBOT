5a0b86ec06d9b7673f0097e832246912970fb647
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-07-11 17:34:36 +0530

    Create Zfonts.py

---
+++ Barath/plugins/Zfonts.py
@@ -0,0 +1,121 @@
+#MIT License
+
+#Copyright (c) 2024 Japanese-X-Userbot
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+# Credit 
+# @Rizzvbss | @Whynan
+# How come?
+# © @KynanSupport
+# REMAKE BY NOBITA XD AND TRYTOLIVEALONE
+
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Barath import *
+from config import CMD_HANDLER
+from Barath.helpers.tools import get_arg
+
+arguments = [
+    "smallcap",
+    "monospace",
+    "outline",
+    "script",
+    "blackbubbles",
+    "bubbles",
+    "bold",
+    "bolditalic"
+]
+
+fonts = [
+    "smallcap",
+    "monospace",
+    "outline",
+    "script",
+    "blackbubbles",
+    "bubbles",
+    "bold",
+    "bolditalic"
+]
+
+_default = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+_smallcap = "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘϙʀsᴛᴜᴠᴡxʏᴢABCDEFGHIJKLMNOPQRSTUVWXYZ"
+_monospace = "𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉"
+_outline = "𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤ"
+_script = "𝒶𝒷𝒸𝒹𝑒𝒻𝑔𝒽𝒾𝒿𝓀𝓁𝓂𝓃𝑜𝓅𝓆𝓇𝓈𝓉𝓊𝓋𝓌𝓍𝓎𝓏𝒜ℬ𝒞𝒟ℰℱ𝒢ℋℐ𝒥𝒦ℒℳ𝒩𝒪𝒫𝒬ℛ𝒮𝒯𝒰𝒱𝒲𝒳𝒴𝒵"
+_blackbubbles = "🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩"
+_bubbles = "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ"
+_bold = "𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭"
+_bolditalic = "𝙖𝙗𝙘𝙙𝙚𝙛𝙜𝙝𝙞𝙟𝙠𝙡𝙢𝙣𝙤𝙥𝙦𝙧𝙨𝙩𝙪𝙫𝙬𝙭𝙮𝙯𝘼𝘽𝘾𝘿𝙀𝙁𝙂𝙃𝙄𝙅𝙆𝙇𝙈𝙉𝙊𝙋𝙌𝙍𝙎𝙏𝙐𝙑𝙒𝙓𝙔𝙕"
+
+
+def gen_font(text, new_font):
+    new_font = " ".join(new_font).split()
+    for q in text:
+        if q in _default:
+            new = new_font[_default.index(q)]
+            text = text.replace(q, new)
+    return text
+
+@Client.on_message(
+    filters.command(["font"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def font_ubot(client: Client, message: Message):
+    if message.reply_to_message or get_arg(message):
+        font = get_arg(message)
+        text = message.reply_to_message.text
+        if not font:
+            return await message.reply(f"<code>{font} Not On List Font...</code>")
+        if font == "smallcap":
+            nan = gen_font(text, _smallcap)
+        elif font == "monospace":
+            nan = gen_font(text, _monospace)
+        elif font == "outline":
+            nan = gen_font(text, _outline)
+        elif font == "script":
+            nan = gen_font(text, _script)
+        elif font == "blackbubbles":
+            nan = gen_font(text, _blackbubbles)
+        elif font == "bubbles":
+            nan = gen_font(text, _bubbles)
+        elif font == "bold":
+            nan = gen_font(text, _bold)
+        elif font == "bolditalic":
+            nan = gen_font(text, _bolditalic)
+        await message.reply(nan)
+    else:
+        return await message.reply("Reply Text And Fill In Font Name!!!")
+
+
+@Client.on_message(
+    filters.command(["lf", "listfont"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def fonts(client: Client, message: Message):
+    await message.reply(
+        "<b>ᴅᴀғᴛᴀʀ ғᴏɴᴛs</b>\n\n"
+        "<b>• smallcap</b>\n"
+        "<b>• monospace</b>\n"
+        "<b>• outline</b>\n"
+        "<b>• script</b>\n"
+        "<b>• blackbubbles</b>\n"
+        "<b>• bubbles</b>\n"
+        "<b>• bold</b>\n"
+        "<b>• bolditalic</b>\n\n"
+    )
