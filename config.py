8c4aba46f87006f55ed6b61f3465de6926623998
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-11-23 09:34:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ SUDO_USERS = os.getenv("SUDO_USERS",1238234357)
 HANDLER = ["~", ".","!","?","@","$"]
 BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
-SOURCE = "https://github.com/SIAmKira/PyroX-V2"
+SOURCE = "GitHub.com/Apex-Society/TEAM-APEX-USERBOT"
 LOG_GROUP = -1002119409366
